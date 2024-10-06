76061503690b6005b935ffd247565fa0bf3c0282
Witchaz <Jengkug23@gmail.com>
2024-10-06 14:11:13 +0700

    feat : accept only name

--- src/customers.ts
+++ src/customers.ts
@@ -7,11 +7,8 @@ app.get("/getByID/:id", async (id: string) => {
   return await db.$queryRaw`SELECT "id","name","surname","company_name","credit_limit","address","tax_id,","tel" FROM "customers" WHERE id like ${id}`;
 });
 
-app.get(
-  "/getIDbynameAndSurnameOrCompanyName/:name/:surname/:company_name",
-  async (name: string, surname: string, company_name: string) => {
-    return await db.$queryRaw`SELECT "id" FROM "customers" WHERE company_name like ${company_name} OR (name like ${name} AND surname like ${surname}) `;
-  }
-);
+app.get("/getIDbynameAndSurnameOrCompanyName/:name/", async (name: string) => {
+  return await db.$queryRaw`SELECT "id" FROM "customers" WHERE "name" like ${name}  `;
+});
 
 export default app;
