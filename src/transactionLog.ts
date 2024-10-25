3d0e17c14035529975b3cc9808d5d1d89e3ea812
Witchaz <Jengkug23@gmail.com>
2024-10-26 03:56:42 +0700

    feat : add transactionLog query api

---
+++ src/transactionLog.ts
@@ -0,0 +1,40 @@
+import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
+import db from "./db";
+
+const app = new Elysia({ prefix: "/transactionLog" });
+
+interface Transaction_log {
+  id: number;
+  spare_parts: number;
+  quantity: number;
+  user_id: number;
+  from_user_id: number;
+  status: number;
+}
+
+app.get("/getList", async () => {
+  return await db.$queryRaw`SELECT "id","spare_parts","quantity","user_id","from_user_id","status" 
+  FROM "Transaction_log"`;
+});
+
+app.post(
+  "/insertTransactionLog",
+  async ({ body }: { body: Transaction_log }) => {
+    try {
+      const { spare_parts, quantity, user_id, from_user_id, status } = body;
+      await db.$queryRaw`INSERT INTO "Transaction_log" ("spare_parts","quantity","user_id","from_user_id","status") 
+      VALUES (${spare_parts}, ${quantity}, ${user_id}, ${from_user_id}, ${status})`;
+      return { message: "Transaction Log inserted successfully." };
+    } catch (error: any) {
+      return {
+        message: "Error inserting Transaction Log.",
+        error: error.message,
+      };
+    }
+  }
+);
+
+//TODO List
+
+export default app;
