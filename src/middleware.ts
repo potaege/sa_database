c4adf7c0cac8a7e8a07d27e47ea66dea4315e4eb
Witchaz <Jengkug23@gmail.com>
2024-10-29 23:52:42 +0700

    fix : reverse config

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,13 @@ const middleware = new Elysia()
     set.headers["access-control-allow-origin"] = "*";
     set.headers["access-control-allow-methods"] =
       "GET, POST, PUT, DELETE, OPTIONS";
-    set.headers["access-control-allow-headers"] = "*";
-    // set.headers["access-control-allow-headers"] = "Content-Type, Authorization";
+    set.headers["access-control-allow-headers"] = "Content-Type, Authorization";
   })
   .options("*", async ({ set }) => {
     set.headers["access-control-allow-origin"] = "*";
     set.headers["access-control-allow-methods"] =
       "GET, POST, PUT, DELETE, OPTIONS";
-    // set.headers["access-control-allow-headers"] = "Content-Type, Authorization";
-    set.headers["access-control-allow-headers"] = "*";
+    set.headers["access-control-allow-headers"] = "Content-Type, Authorization";
     set.status = 204; // No content
     return null;
   });
