f460aa0a87f9650ed7fe22d83adf9249c715c757
Witchaz <Jengkug23@gmail.com>
2024-11-02 17:18:00 +0700

    fix : naming additionalCosts -> AdditionalCosts

--- src/additionalcosts.ts
+++ src/additionalcosts.ts
@@ -17,7 +17,7 @@ interface Additionalcost {
 }
 
 app.get("/getFromWorkId/:workid", async ({ params }) => {
-  return await db.$queryRaw`SELECT "id","description","cost","amount","unit","work_id","add_date" FROM "additional_costs" WHERE "work_id" = ${parseInt(
+  return await db.$queryRaw`SELECT "id","description","cost","amount","unit","work_id","add_date" FROM "AdditionalCosts" WHERE "work_id" = ${parseInt(
     params.workid
   )};`;
 });
@@ -28,7 +28,7 @@ app.post(
     try {
       const { description, cost, amount, unit, work_id } = body;
 
-      await db.$queryRaw`INSERT INTO "additional_costs" ("description","cost","amount","unit","work_id") VALUES (${description},${cost},${amount},${unit},${work_id})`;
+      await db.$queryRaw`INSERT INTO "AdditionalCosts" ("description","cost","amount","unit","work_id") VALUES (${description},${cost},${amount},${unit},${work_id})`;
       return "add new additional cost";
     } catch (error: any) {
       return {
@@ -54,7 +54,7 @@ app.post(
     try {
       const { id, description, cost, amount, unit, work_id } = body;
 
-      await db.$queryRaw`UPDATE "additional_costs" SET "description" = ${description}, "cost" = ${cost}, "amount" = ${amount}, "unit" = ${unit}, "work_id" = ${work_id} WHERE "id" = ${id}`;
+      await db.$queryRaw`UPDATE "AdditionalCosts" SET "description" = ${description}, "cost" = ${cost}, "amount" = ${amount}, "unit" = ${unit}, "work_id" = ${work_id} WHERE "id" = ${id}`;
       return "edit additional cost";
     } catch (error: any) {
       return {
@@ -81,7 +81,7 @@ app.post(
   async ({ body }: { body: Additionalcost }) => {
     try {
       const { id } = body;
-      await db.$queryRaw`DELETE FROM "additional_costs" WHERE "id" = ${id}`;
+      await db.$queryRaw`DELETE FROM "AdditionalCosts" WHERE "id" = ${id}`;
       return "delete additional cost";
     } catch (error: any) {
       return {
