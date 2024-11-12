0a8160182087c26c7f628a33b558f8004827998b
Witchaz <Jengkug23@gmail.com>
2024-11-12 19:53:19 +0700

    feat  : add spare part engineer return id

--- src/spare_parts_engineer.ts
+++ src/spare_parts_engineer.ts
@@ -14,7 +14,7 @@ interface SparePartEngineerTable {
   add_date: Date;
 }
 app.get("/getFromUserID/:userID", async ({ params }) => {
-  return await db.$queryRaw`SELECT Spare_parts_engineers.id,Spare_parts.name,Spare_parts_engineers.quantity,Spare_parts_engineers.user_id,Spare_parts_engineers."add_date"
+  return await db.$queryRaw`SELECT Spare_parts.id,Spare_parts_engineers.id,Spare_parts.name,Spare_parts_engineers.quantity,Spare_parts_engineers.user_id,Spare_parts_engineers."add_date"
     FROM "Spare_parts_engineers" AS Spare_parts_engineers
     JOIN "Spare_parts" AS Spare_parts ON Spare_parts.id = Spare_parts_engineers.spare_part_id
     WHERE Spare_parts_engineers."user_id" = ${parseInt(params.userID)}`;
